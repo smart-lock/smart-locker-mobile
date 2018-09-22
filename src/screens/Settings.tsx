b4932a95fc1d173be31f236e0e998dd1aa31f13c
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-22 15:16:00 -0300

    Logot button

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -2,18 +2,28 @@ import React from 'react'
 import { ScreenWrapper } from '../components/ScreenWrapper';
 import { Text } from '../components/Text';
 import { basicStackScreenNavigationOptions } from '../resources/styles';
+import AwesomeButton from 'react-native-really-awesome-button'
+import { NavigationInjectedProps } from 'react-navigation';
+import { RootScreens } from '../navigation/RootNavigator';
 
-export class SettingsScreen extends React.Component {
+export class SettingsScreen extends React.Component<NavigationInjectedProps> {
   static navigationOptions = basicStackScreenNavigationOptions({
     title: 'Configurações'
   })
 
+  private handlePress = () => {
+    this.props.navigation.navigate(RootScreens.SignIn)
+  }
   render() {
     return (
       <ScreenWrapper>
-        <Text>
-          Settings
-        </Text>
+        <AwesomeButton
+          onPress={this.handlePress}
+          style={{marginTop: 20, alignSelf: 'center'}}
+          backgroundColor='#fc5c65'
+          backgroundDarker="#eb3b5a">
+          <Text style={{fontWeight: '400', color: '#fff', fontSize: 20}}>SAIR</Text>
+        </AwesomeButton>
       </ScreenWrapper>
     )
   }
