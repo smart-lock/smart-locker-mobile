9911b5c72b1b4d499c03e307e24fe3d9b1cdc3d7
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-23 15:55:27 -0300

    LockButton options

--- src/components/LockButton.tsx
+++ src/components/LockButton.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import AwesomeButton from 'react-native-really-awesome-button'
-import { View, Text, ViewStyle } from 'react-native'
+import { View, Text, ViewStyle, ActivityIndicator } from 'react-native'
 import EvilIcons from 'react-native-vector-icons/EvilIcons'
 
 export interface LockButtonProps {
   onPress: () => void;
   locked: boolean;
   disabled: boolean;
+  activeLabel: string
+  inactiveLabel: string
+  activeIcon: string
+  inactiveIcon: string
+  loading: boolean
+  width?: number
+  height?: number
   style?: ViewStyle
 }
 
@@ -41,27 +48,38 @@ export const LockButton: React.SFC<LockButtonProps> = ({
   locked,
   disabled,
   style,
+  activeLabel,
+  inactiveLabel,
+  activeIcon,
+  inactiveIcon,
+  width,
+  height,
+  loading
 }) => {
 
   const theme = getTheme(locked, disabled)
-  const text = locked ? 'UNLOCK' : 'LOCK'
-  const iconName = locked ? 'unlock' : 'unlock'
+  const text = locked ? activeLabel : inactiveLabel
+  const iconName = locked ? activeIcon : inactiveIcon
 
   return (
     <AwesomeButton
+      raiseLevel={loading ? 0 : undefined}
       backgroundColor={theme.backgroundColor}
       backgroundActive={theme.backgroundActive}
       backgroundDarker={theme.backgroundDarker}
       onPress={onPress}
       disabled={disabled}
-      borderRadius={100}
-      height={200}
-      width={200}
+      height={height}
+      width={width}
       style={style}>
-      <View style={{flexDirection: 'column', alignItems: 'center'}}>
-        <EvilIcons name={iconName} size={60} color="#FFF" />
-        <Text style={{fontWeight: '400', color: '#fff', fontSize: 20}}>{text}</Text>
-      </View>
+        {loading ? (
+          <ActivityIndicator color='#FFF'/>
+        ): (
+          <View style={{flexDirection: 'column', alignItems: 'center'}}>
+            <EvilIcons name={iconName} size={60} color="#FFF" />
+            <Text style={{fontWeight: '400', color: '#fff', fontSize: 20}}>{text}</Text>
+          </View>
+        )}
     </AwesomeButton>
   )
   
