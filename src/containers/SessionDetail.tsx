01fc9de648316deb500cb07ae5f6969f30ee4436
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-23 15:55:49 -0300

    SessionDetailContainer mutations

--- src/containers/SessionDetail.tsx
+++ src/containers/SessionDetail.tsx
@@ -3,25 +3,20 @@ import { Query } from 'react-apollo'
 import { ActivityIndicator, Alert } from 'react-native';
 import { LOCKER_SESSION_QUERY } from '../graphql/queries';
 import { LockerSessionDetail } from '../components/SessionDetail';
+import { apolloClient } from '../apollo';
+import { LockMutation, UnlockMutation, UnclaimMutation } from '../graphql/mutations';
+
 
 export interface ISessionDetailContainerProps {
   lockerSessionId: string
 }
 
+
 export class SessionDetailContainer extends React.Component<ISessionDetailContainerProps> {
-  private handlePressLock = () => {
-    Alert.alert('lock!')
-  }
-
-  private handlePressUnlock = () => {
-    Alert.alert('unlock!')
-  }
-
   render() {
     const {
       lockerSessionId
     } = this.props
-    console.log(lockerSessionId)
     return (
       <Query
         query={LOCKER_SESSION_QUERY}
@@ -36,16 +31,35 @@ export class SessionDetailContainer extends React.Component<ISessionDetailContai
             )
           }
           console.log(data)
-          if (data) {
+          if (data && data.lockerSession) {
             const { lockerSession } = data
+            const { id: lockerId } = lockerSession.locker
             return (
-              <LockerSessionDetail
-                id={lockerSession.id}
-                startedAt={lockerSession.startedAt}
-                locker={lockerSession.locker}
-                onPressLock={this.handlePressLock}
-                onPressUnlock={this.handlePressUnlock}
-              />
+              <LockMutation variables={{lockerId}}>
+                {(lockMutation, { loading: lockLoading }) => (
+                  <UnlockMutation variables={{lockerId}}>
+                    {(unlockMutation, { loading: unlockLoading }) => (
+                      <UnclaimMutation variables={{lockerId}}>
+                        {(unclaimMutation, { loading: unclaimLoading }) => (
+                          <LockerSessionDetail
+                            id={lockerSession.id}
+                            startedAt={lockerSession.startedAt}
+                            locker={lockerSession.locker}
+                            onPressLock={() => lockMutation()}
+                            onPressUnlock={() => {
+                              unlockMutation()
+                            }}
+                            onPressUnclaim={() => unclaimMutation()}
+                            lockLoading={lockLoading}
+                            unlockLoading={unlockLoading}
+                            unclaimLoading={unclaimLoading}
+                          />
+                        )}
+                      </UnclaimMutation>
+                    )}
+                  </UnlockMutation>
+                )} 
+              </LockMutation>
             )
           }
           return null
