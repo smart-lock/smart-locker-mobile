26eabedbc10f92b9ea609069f93e0c7ab332509e
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-23 16:53:58 -0300

    Fix charge logic

--- src/logic/charge.ts
+++ src/logic/charge.ts
@@ -4,5 +4,5 @@ const PRICE_PER_HOUR = 1000
 
 export const getChargeBetweenDates = (final: Date, since: Date): number => {
   const hours = hoursPassedSince(final, since)
-  return Math.max(1, hours) * PRICE_PER_HOUR
+  return hours * PRICE_PER_HOUR
 }
