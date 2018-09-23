63316444c2fe6927170cf25eefa1b4483f4d3d43
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-22 22:29:19 -0300

    LockerGrid real props

--- src/components/LockerGrid.tsx
+++ src/components/LockerGrid.tsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import GridView from 'react-native-super-grid';
-import { Text } from './Text';
-import { View, Dimensions } from 'react-native';
-import { Locker, lockerSpacing, lockerWidth } from './Locker';
+import { LockerSession, lockerSpacing, lockerWidth, ILockerSession } from './Locker';
 
-const { width } = Dimensions.get('window')
-
-export interface ILocker {
-  id: string
-}
 export interface ILockerGrid {
-  lockers: ILocker[]
-  onPressLocker: (lockerId: string) => void
+  sessions: ILockerSession[]
+  onPressLockerSession: (lockerSessionId: string) => void
 }
-export const LockerGrid: React.SFC<ILockerGrid> = ({
-  lockers,
-  onPressLocker,
+export const LockerSessionsGrid: React.SFC<ILockerGrid> = ({
+  sessions,
+  onPressLockerSession,
 }) => (
   <GridView
     itemDimension={lockerWidth}
     spacing={lockerSpacing}
-    items={['1','2','3','4','5','6']}
-    renderItem={(item: string) => (
-      <Locker id={item} onPress={() => onPressLocker(item)}/>
+    items={sessions}
+    renderItem={(lockerSession: ILockerSession) => (
+      <LockerSession
+        {...lockerSession}
+        onPress={() => onPressLockerSession(lockerSession.id)}
+      />
     )}
   />
 )
