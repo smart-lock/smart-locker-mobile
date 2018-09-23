09964a7f51f52d60b71045b432a32cfe430c806a
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-23 15:56:22 -0300

    Claimlocker screen

--- src/screens/ClaimLockerScreen.tsx
+++ src/screens/ClaimLockerScreen.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { ScreenWrapper } from '../components/ScreenWrapper';
-import { Text } from '../components/Text';
 import { basicStackScreenNavigationOptions } from '../resources/styles';
-import { RNCamera } from 'react-native-camera';
+import { ClaimLockerContainer } from '../containers/ClaimLocker';
+import { MyLockersScreens } from '../navigation/MyLockersNavigator';
 
 export class ClaimLockerScreen extends React.Component {
   static navigationOptions = basicStackScreenNavigationOptions({
     title: 'Alugar armário'
   })
 
+  private navigateToSessionDetail = (lockerSessionId: string) => {
+    this.props.navigation.navigate(MyLockersScreens.SessionDetail, {
+      lockerSessionId,
+    })
+  }
+
   render() {
     return (
       <ScreenWrapper>
-        <RNCamera
-          style={{flex: 1}}
-        />
+        <ClaimLockerContainer navigateToSessionDetail={this.navigateToSessionDetail} />
       </ScreenWrapper>
     )
   }
