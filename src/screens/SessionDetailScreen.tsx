55c8a45c730b512a19b30de9f20b1cae24c4aa1e
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-23 16:05:25 -0300

    navigateToMyLockers fn

--- src/screens/SessionDetailScreen.tsx
+++ src/screens/SessionDetailScreen.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { basicStackScreenNavigationOptions } from '../resources/styles';
 import { SessionDetailContainer } from '../containers/SessionDetail';
 import { NavigationInjectedProps } from 'react-navigation';
+import { MyLockersScreens } from '../navigation/MyLockersNavigator';
 
 export class SessionDetailScreen extends React.Component<NavigationInjectedProps> {
   static navigationOptions = basicStackScreenNavigationOptions({
     title: 'Armário'
   })
   
+  private navigateToMyLockers = () => {
+    this.props.navigation.navigate(MyLockersScreens.MyLockers)
+  }
   render() {
     const lockerSessionId = this.props.navigation.getParam('lockerSessionId')
     return (
       <SessionDetailContainer
+        navigateToMyLockers={this.navigateToMyLockers}
         lockerSessionId={lockerSessionId}
       />
     )
