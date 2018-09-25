dd05956aefa2f785d032bff3bfd08a76b639eb7f
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-25 01:44:53 -0300

    Reading qrcode propertly

--- src/components/ClaimLockerCamera.tsx
+++ src/components/ClaimLockerCamera.tsx
@@ -2,31 +2,35 @@ import React from 'react'
 
 
 import { RNCamera, BarCodeType } from 'react-native-camera';
-import { Button } from 'react-native';
+import { Button, Alert } from 'react-native';
 
 export interface IClaimLockerCameraProps {
   onLockerRead: (lockerId: string) => void
+  renderCamera: boolean
 }
 
 export interface IBarcodeReadEvent {
   data: string,
-  type: keyof BarCodeType,
+  type: any,
 }
 
 const fakeRead = ['cjmdtr3q3000s0a5842xwqcy1', 'cjmdtr3qd000u0a587jl0p7mu']
 
 export class ClaimLockerCamera extends React.Component<IClaimLockerCameraProps> {
   private handleBarcodeRead = ({ data, type }: IBarcodeReadEvent) => {
-    if (type === 'qr') {
+    if (type === 'QR_CODE') {
       this.props.onLockerRead(data)
     }
   }
   render() {
+    if (!this.props.renderCamera) {
+      return null
+    }
     return (
       <RNCamera
         onBarCodeRead={this.handleBarcodeRead}
         style={{flex: 1}}>
-          {fakeRead.map(fakeRead => (
+          {/* {fakeRead.map(fakeRead => (
             <Button
               key={fakeRead}
               title={fakeRead}
@@ -34,7 +38,7 @@ export class ClaimLockerCamera extends React.Component<IClaimLockerCameraProps> 
                 this.props.onLockerRead(fakeRead)
               }}
             />
-          ))}
+          ))} */}
       </RNCamera>
     )
   }
